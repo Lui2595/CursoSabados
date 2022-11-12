11446483cf466bd39ad3a39b4a9b431eec73236e
LuisRivka <luis.p@rivka.mx>
2022-11-12 15:09:17 -0600

    Update

--- tienda/inc/top.php
+++ tienda/inc/top.php
@@ -32,6 +32,9 @@
           <li class="nav-item">
             <a class="nav-link" href="proveedores.php">Proveedores</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="productos.php">Productos</a>
+          </li>
           <!-- <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Dropdown
